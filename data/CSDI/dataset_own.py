8d6255dfffab38936658b4f1fe8ef4484beb3e43
ZONG0004 <55400087+ZONG0004@users.noreply.github.com>
2022-09-16 11:48:23 +0800

    Add files via upload

---
+++ data/CSDI/dataset_own.py
@@ -0,0 +1,174 @@
+import pickle
+import pdb
+import os
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+
+
+attributes = ['open', 'high', 'low', 'close', 'adjcp']
+
+
+def parse_data(x):
+    x = x.set_index("Attribute").to_dict()["Value"]
+
+    values = []
+
+    for attr in attributes:
+        if x.__contains__(attr):
+            values.append(x[attr])
+        else:
+            values.append(np.nan)
+    return values
+
+
+def parse_id(missing_ratio=0.1, dataset_name = "dj30", tic_name="AAPL"):
+    dataset_path = "./source/" + dataset_name + ".csv"
+    data = pd.read_csv(dataset_path)
+    date_list = data.date.unique().tolist()
+    start_date = date_list[0]
+    end_date = date_list[-1]
+    date_list_2 = pd.bdate_range(start=start_date, end=end_date)
+    df = data.loc[data['tic']==tic_name]
+    df = df[['date', 'open', 'high', 'low', 'close', 'adjcp']]
+    df = pd.melt(df, id_vars=["date"], value_vars=['date', 'open', 'high', 'low', 'close', 'adjcp'], var_name="Attribute", value_name="Value")
+    df = df.sort_values(by=["date"])
+    date = [ x.strftime('%F') for x in date_list_2]
+    observed_values = []
+    for d in date:
+        observed_values.append(parse_data(df[df["date"] == d]))
+    observed_values = np.array(observed_values)
+    observed_masks = ~np.isnan(observed_values)
+    masks = observed_masks.reshape(-1).copy()
+    obs_indices = np.where(masks)[0].tolist()
+    miss_indices = np.random.choice(
+        obs_indices, (int)(len(obs_indices) * missing_ratio), replace=False
+    )
+    #pdb.set_trace()
+    masks[miss_indices] = False
+    gt_masks = masks.reshape(observed_masks.shape)
+
+    observed_values = np.nan_to_num(observed_values)
+    observed_masks = observed_masks.astype("float32")
+    gt_masks = gt_masks.astype("float32")
+    return observed_values, observed_masks, gt_masks
+
+
+
+
+class Own_Dataset(Dataset):
+    def __init__(self, eval_length=10, use_index_list=None, missing_ratio=0.0, seed=0, dataset_name = "dj30", tic_name = "AAPL"):
+        self.eval_length = eval_length
+        np.random.seed(seed)  
+        self.observed_values = []
+        self.observed_masks = []
+        self.gt_masks = []
+        path = (
+            "./data/" + dataset_name + "/" + tic_name + "_missing" + str(missing_ratio) + "_seed" + str(seed) + ".pk"
+        )
+
+        if os.path.isfile(path) == False:  
+
+            observed_values, observed_masks, gt_masks = parse_id(
+                        missing_ratio, dataset_name, tic_name
+                )
+            self.observed_values.append(observed_values)
+            self.observed_masks.append(observed_masks)
+            self.gt_masks.append(gt_masks)
+
+            self.observed_values = np.array(self.observed_values)
+            self.observed_masks = np.array(self.observed_masks)
+            self.gt_masks = np.array(self.gt_masks)
+
+            tmp_values = self.observed_values.reshape(-1, 5)
+            tmp_masks = self.observed_masks.reshape(-1, 5)
+            mean = np.zeros(5)
+            std = np.zeros(5)
+            for k in range(5):
+                c_data = tmp_values[:, k][tmp_masks[:, k] == 1]
+                mean[k] = c_data.mean()
+                std[k] = c_data.std()
+            
+            self.observed_values = (
+                (self.observed_values - mean) / std * self.observed_masks
+            )
+
+            with open(path, "wb") as f:
+                pickle.dump(
+                    [self.observed_values, self.observed_masks, self.gt_masks, mean, std], f
+                )
+            with open(path, "rb") as f:
+                self.observed_values, self.observed_masks, self.gt_masks, mean, std= pickle.load(
+                    f
+                )
+            data_num = self.observed_values[0].shape[0]
+            data_deleted = data_num % 10
+            
+            self.observed_values = np.delete(self.observed_values[0], np.s_[-data_deleted: ], axis = 0)
+            self.observed_values = np.reshape(self.observed_values, (data_num // 10, 10, 5))
+            self.observed_masks = np.delete(self.observed_masks[0], np.s_[-data_deleted: ], axis = 0)
+            self.observed_masks = np.reshape(self.observed_masks, (data_num // 10, 10, 5))
+            self.gt_masks = np.delete(self.gt_masks[0], np.s_[-data_deleted: ], axis = 0)
+            self.gt_masks = np.reshape(self.gt_masks, (data_num // 10, 10, 5))
+        else:  
+            with open(path, "rb") as f:
+                self.observed_values, self.observed_masks, self.gt_masks, mean, std= pickle.load(
+                    f
+                )
+            data_num = self.observed_values[0].shape[0]
+            data_deleted = data_num % 10
+            self.observed_values = np.delete(self.observed_values[0], np.s_[-data_deleted: ], axis = 0)
+            self.observed_values = np.reshape(self.observed_values, (data_num // 10, 10, 5))
+            self.observed_masks = np.delete(self.observed_masks[0], np.s_[-data_deleted: ], axis = 0)
+            self.observed_masks = np.reshape(self.observed_masks, (data_num // 10, 10, 5))
+            self.gt_masks = np.delete(self.gt_masks[0], np.s_[-data_deleted: ], axis = 0)
+            self.gt_masks = np.reshape(self.gt_masks, (data_num // 10, 10, 5))
+        if use_index_list is None:
+            self.use_index_list = np.arange(len(self.observed_values))
+        else:
+            self.use_index_list = use_index_list
+
+    def __getitem__(self, org_index):
+        index = self.use_index_list[org_index]
+        s = {
+            "observed_data": self.observed_values[index],
+            "observed_mask": self.observed_masks[index],
+            "gt_mask": self.gt_masks[index],
+            "timepoints": np.arange(self.eval_length),
+        }
+        return s
+
+    def __len__(self):
+        return len(self.use_index_list)
+
+
+def get_dataloader(seed=1, batch_size=16, missing_ratio=0.1, dataset_name = "dj30", tic_name = "AAPL"):
+
+
+    dataset = Own_Dataset(missing_ratio=missing_ratio, seed=seed, dataset_name = dataset_name, tic_name = tic_name)
+    indlist = np.arange(len(dataset))
+
+    np.random.seed(seed)
+
+    start = 0
+    end = len(dataset)
+    test_index = indlist[start:end]
+    remain_index = indlist
+    np.random.seed(seed)
+    num_train = (int)(len(dataset) * 0.8)
+    train_index = remain_index[:num_train]
+    valid_index = remain_index[num_train:]
+
+    dataset = Own_Dataset(
+        use_index_list=train_index, missing_ratio=missing_ratio, seed=seed, dataset_name = dataset_name, tic_name = tic_name
+    )
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=1)
+    valid_dataset = Own_Dataset(
+        use_index_list=valid_index, missing_ratio=missing_ratio, seed=seed, dataset_name = dataset_name, tic_name = tic_name
+    )
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=0)
+    test_dataset = Own_Dataset(
+        use_index_list=test_index, missing_ratio=missing_ratio, seed=seed, dataset_name = dataset_name, tic_name = tic_name
+    )
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=0)
+    return train_loader, valid_loader, test_loader
---
+++ data/CSDI/diff_models.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+def get_torch_trans(heads=8, layers=1, channels=64):
+    encoder_layer = nn.TransformerEncoderLayer(
+        d_model=channels, nhead=heads, dim_feedforward=64, activation="gelu"
+    )
+    return nn.TransformerEncoder(encoder_layer, num_layers=layers)
+
+
+def Conv1d_with_init(in_channels, out_channels, kernel_size):
+    layer = nn.Conv1d(in_channels, out_channels, kernel_size)
+    nn.init.kaiming_normal_(layer.weight)
+    return layer
+
+
+class DiffusionEmbedding(nn.Module):
+    def __init__(self, num_steps, embedding_dim=128, projection_dim=None):
+        super().__init__()
+        if projection_dim is None:
+            projection_dim = embedding_dim
+        self.register_buffer(
+            "embedding",
+            self._build_embedding(num_steps, embedding_dim / 2),
+            persistent=False,
+        )
+        self.projection1 = nn.Linear(embedding_dim, projection_dim)
+        self.projection2 = nn.Linear(projection_dim, projection_dim)
+
+    def forward(self, diffusion_step):
+        x = self.embedding[diffusion_step]
+        x = self.projection1(x)
+        x = F.silu(x)
+        x = self.projection2(x)
+        x = F.silu(x)
+        return x
+
+    def _build_embedding(self, num_steps, dim=64):
+        steps = torch.arange(num_steps).unsqueeze(1)  # (T,1)
+        frequencies = 10.0 ** (torch.arange(dim) / (dim - 1) * 4.0).unsqueeze(0)  # (1,dim)
+        table = steps * frequencies  # (T,dim)
+        table = torch.cat([torch.sin(table), torch.cos(table)], dim=1)  # (T,dim*2)
+        return table
+
+
+class diff_CSDI(nn.Module):
+    def __init__(self, config, inputdim=2):
+        super().__init__()
+        self.channels = config["channels"]
+
+        self.diffusion_embedding = DiffusionEmbedding(
+            num_steps=config["num_steps"],
+            embedding_dim=config["diffusion_embedding_dim"],
+        )
+
+        self.input_projection = Conv1d_with_init(inputdim, self.channels, 1)
+        self.output_projection1 = Conv1d_with_init(self.channels, self.channels, 1)
+        self.output_projection2 = Conv1d_with_init(self.channels, 1, 1)
+        nn.init.zeros_(self.output_projection2.weight)
+
+        self.residual_layers = nn.ModuleList(
+            [
+                ResidualBlock(
+                    side_dim=config["side_dim"],
+                    channels=self.channels,
+                    diffusion_embedding_dim=config["diffusion_embedding_dim"],
+                    nheads=config["nheads"],
+                )
+                for _ in range(config["layers"])
+            ]
+        )
+
+    def forward(self, x, cond_info, diffusion_step):
+        B, inputdim, K, L = x.shape
+
+        x = x.reshape(B, inputdim, K * L)
+        x = self.input_projection(x)
+        x = F.relu(x)
+        x = x.reshape(B, self.channels, K, L)
+
+        diffusion_emb = self.diffusion_embedding(diffusion_step)
+
+        skip = []
+        for layer in self.residual_layers:
+            x, skip_connection = layer(x, cond_info, diffusion_emb)
+            skip.append(skip_connection)
+
+        x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers))
+        x = x.reshape(B, self.channels, K * L)
+        x = self.output_projection1(x)  # (B,channel,K*L)
+        x = F.relu(x)
+        x = self.output_projection2(x)  # (B,1,K*L)
+        x = x.reshape(B, K, L)
+        return x
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, side_dim, channels, diffusion_embedding_dim, nheads):
+        super().__init__()
+        self.diffusion_projection = nn.Linear(diffusion_embedding_dim, channels)
+        self.cond_projection = Conv1d_with_init(side_dim, 2 * channels, 1)
+        self.mid_projection = Conv1d_with_init(channels, 2 * channels, 1)
+        self.output_projection = Conv1d_with_init(channels, 2 * channels, 1)
+
+        self.time_layer = get_torch_trans(heads=nheads, layers=1, channels=channels)
+        self.feature_layer = get_torch_trans(heads=nheads, layers=1, channels=channels)
+
+    def forward_time(self, y, base_shape):
+        B, channel, K, L = base_shape
+        if L == 1:
+            return y
+        y = y.reshape(B, channel, K, L).permute(0, 2, 1, 3).reshape(B * K, channel, L)
+        y = self.time_layer(y.permute(2, 0, 1)).permute(1, 2, 0)
+        y = y.reshape(B, K, channel, L).permute(0, 2, 1, 3).reshape(B, channel, K * L)
+        return y
+
+    def forward_feature(self, y, base_shape):
+        B, channel, K, L = base_shape
+        if K == 1:
+            return y
+        y = y.reshape(B, channel, K, L).permute(0, 3, 1, 2).reshape(B * L, channel, K)
+        y = self.feature_layer(y.permute(2, 0, 1)).permute(1, 2, 0)
+        y = y.reshape(B, L, channel, K).permute(0, 2, 3, 1).reshape(B, channel, K * L)
+        return y
+
+    def forward(self, x, cond_info, diffusion_emb):
+        B, channel, K, L = x.shape
+        base_shape = x.shape
+        x = x.reshape(B, channel, K * L)
+
+        diffusion_emb = self.diffusion_projection(diffusion_emb).unsqueeze(-1)  # (B,channel,1)
+        y = x + diffusion_emb
+
+        y = self.forward_time(y, base_shape)
+        y = self.forward_feature(y, base_shape)  # (B,channel,K*L)
+        y = self.mid_projection(y)  # (B,2*channel,K*L)
+
+        _, cond_dim, _, _ = cond_info.shape
+        cond_info = cond_info.reshape(B, cond_dim, K * L)
+        cond_info = self.cond_projection(cond_info)  # (B,2*channel,K*L)
+        y = y + cond_info
+
+        gate, filter = torch.chunk(y, 2, dim=1)
+        y = torch.sigmoid(gate) * torch.tanh(filter)  # (B,channel,K*L)
+        y = self.output_projection(y)
+
+        residual, skip = torch.chunk(y, 2, dim=1)
+        x = x.reshape(base_shape)
+        residual = residual.reshape(base_shape)
+        skip = skip.reshape(base_shape)
+        return (x + residual) / math.sqrt(2.0), skip
