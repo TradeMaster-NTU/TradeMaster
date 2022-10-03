5028a32d265417b4ca210b5a36dff52fd9d50705
qml <937883273@qq.com>
2022-10-03 10:34:36 +0800

    update visulization

--- agent/DeepScalper/test.py
+++ agent/DeepScalper/test.py
@@ -53,26 +53,22 @@
 # config = vars(args)
 # print(config)
 
-
 # def save_dict_to_yaml(dict_value: dict, save_path: str):
 #     """dict保存为yaml"""
 #     with open(save_path, 'w') as file:
 #         file.write(yaml.dump(dict_value, allow_unicode=True))
 
-
 # save_dict_to_yaml(
 #     config,
 #     "/home/sunshuo/qml/TradeMaster_reframe/input_config/env/AT/AT/test.yml")
-
 
 # def read_yaml_to_dict(yaml_path: str, ):
 #     with open(yaml_path) as file:
 #         dict_value = yaml.load(file.read(), Loader=yaml.FullLoader)
 #         return dict_value
 
-
 # print(read_yaml_to_dict("input_config/env/AT/AT/test.yml"))
-a=[0,1,2,3,4,5,6]
+a = [0, 1, 2, 3, 4, 5, 6]
 print(a[0:len(a):2])
-print(0.1<0.2 and 0.1>=0)
-print(sum([[0.1,0.2],[0.3]]))
+print(0.1 < 0.2 and 0.1 >= 0)
+print(sum([[0.1, 0.2], [0.3]]))
