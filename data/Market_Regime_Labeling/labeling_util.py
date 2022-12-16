b4c47ce6d348d13134f91e5af0d1f9192f5a1886
Xiahaochong98 <xiahaochong981002@gmail.com>
2022-12-16 20:58:40 +0800

    add regime_num and length constrain

--- data/Market_Regime_Labeling/labeling_util.py
+++ data/Market_Regime_Labeling/labeling_util.py
@@ -11,16 +11,18 @@ from sklearn.linear_model import LinearRegression
 from random import sample
 import fractions
 import os
+from sklearn.manifold import TSNE
 
 class Labeler():
     def __init__(self,data,method='linear',parameters=['2/7','2/14','4']):
+        plt.ioff()
         self.preprocess(data)
         if method=='linear':
             self.method='linear'
             self.Wn_adjcp, self.Wn_pct, self.order =[float(fractions.Fraction(x)) for x in parameters]
         else:
             raise Exception("Sorry, only linear model is provided for now.")
-    def fit(self):
+    def fit(self,regime_number,length_limit):
         if self.method=='linear':
             for tic in self.tics:
                 self.adjcp_apply_filter(self.data_dict[tic], self.Wn_adjcp, self.Wn_pct, self.order)
@@ -28,15 +30,17 @@ class Labeler():
             self.coef_list_dict = {}
             self.norm_coef_list_dict = {}
             self.y_pred_dict = {}
+            self.regime_number=regime_number
+            self.length_limit=length_limit
             for tic in self.tics:
                 coef_list, turning_points, y_pred_list, norm_coef_list = self.linear_regession_turning_points(
-                    data_ori=self.data_dict[tic], tic=tic)
+                    data_ori=self.data_dict[tic], tic=tic,length_constrain=self.length_limit)
                 self.turning_points_dict[tic] = turning_points
                 self.coef_list_dict[tic] = coef_list
                 self.y_pred_dict[tic] = y_pred_list
                 self.norm_coef_list_dict[tic] = norm_coef_list
 
-    def label(self,parameters=[-0.5,0.5]):
+    def label(self,parameters):
         # return a dict of label where key is the ticker and value is the label of time-series
         if self.method=='linear':
             try:
@@ -44,16 +48,41 @@ class Labeler():
             except:
                 raise Exception(
                     "parameters shoud be [low,high] where the series would be split into 4 regimes by low,high and 0 as threshold based on slope. A value of -0.5 and 0.5 stand for -0.5% and 0.5% change per step.")
+            self.all_data_seg = []
+            self.all_label_seg = []
+            self.all_index_seg = []
             for tic in self.tics:
                 turning_points = self.turning_points_dict[tic]
                 norm_coef_list = self.norm_coef_list_dict[tic]
-                label = self.linear_regession_label(turning_points, low, high, norm_coef_list)
+                label,data_seg,label_seg,index_seg = self.linear_regession_label(self.data_dict[tic],turning_points, low, high, norm_coef_list,tic,self.regime_number)
                 self.data_dict[tic]['label'] = label
-    def linear_regession_label(self, turning_points, low, high, normalized_coef_list):
+                self.all_data_seg.extend(data_seg)
+                self.all_label_seg.extend(label_seg)
+                self.all_index_seg.extend(index_seg)
+            interpolated_pct_return_data_seg = np.array(self.interpolation(self.all_data_seg))
+            self.TSNE_run(interpolated_pct_return_data_seg)
+
+    def linear_regession_label(self,data, turning_points, low, high, normalized_coef_list, tic,
+                               regime_num=4):
+        data = data.reset_index(drop=True)['pct_return_filtered']
+        data_seg = []
         seg1, seg2, seg3 = sorted([low, high, 0])
         label = []
+        label_seg = []
+        index_seg = []
         for i in range(len(turning_points) - 1):
             coef = normalized_coef_list[i]
+            flag = self.regime_flag(regime_num, coef, [seg1, seg2, seg3])
+            label.extend([flag] * (turning_points[i + 1] - turning_points[i]))
+            if turning_points[i + 1] - turning_points[i] > 2:
+                data_seg.append(data.iloc[turning_points[i]:turning_points[i + 1]].to_list())
+                label_seg.append(flag)
+                index_seg.append(tic + '_' + str(i))
+        return label, data_seg, label_seg, index_seg
+
+    def regime_flag(self,regime_num, coef, parameters):
+        seg1, seg2, seg3 = parameters
+        if regime_num == 4:
             if coef <= seg1:
                 flag = 0
             elif coef > seg1 and coef <= seg2:
@@ -62,10 +91,16 @@ class Labeler():
                 flag = 2
             elif coef > seg3:
                 flag = 3
-            label.extend([flag] * (turning_points[i + 1] - turning_points[i]))
-        return label
-
-
+        elif regime_num == 3:
+            if coef <= seg1:
+                flag = 0
+            elif coef > seg1 and coef <= seg3:
+                flag = 1
+            elif coef > seg3:
+                flag = 2
+        else:
+            raise Exception('This regime num is currently not supported')
+        return flag
     def preprocess(self,data):
         data = pd.read_csv(data)
         self.tics = data['tic'].unique()
@@ -173,15 +208,21 @@ class Labeler():
             turning_points.append(data['pct_return_filtered'].size)
         return turning_points
 
-    def linear_regession_turning_points(self,data_ori, tic):
+    def linear_regession_turning_points(self,data_ori, tic,length_constrain=0):
         data = data_ori.reset_index(drop=True)
         turning_points = self.find_index_of_turning(data)
+        turning_points_new = [turning_points[0]]
+        if length_constrain != 0:
+            for i in range(1, len(turning_points) - 1):
+                if turning_points[i] - turning_points_new[-1] >= length_constrain:
+                    turning_points_new.append(turning_points[i])
+            turning_points_new.append(turning_points[-1])
+            turning_points = turning_points_new
         coef_list = []
         normalized_coef_list = []
         y_pred_list = []
         for i in range(len(turning_points) - 1):
             x_seg = np.asarray([j for j in range(turning_points[i], turning_points[i + 1])]).reshape(-1, 1)
-            #         print(x_seg,data['adjcp_filtered'].iloc[turning_points[i]:turning_points[i+1]])
             adj_cp_model = LinearRegression().fit(x_seg,
                                                   data['adjcp_filtered'].iloc[turning_points[i]:turning_points[i + 1]])
             y_pred = adj_cp_model.predict(x_seg)
@@ -189,6 +230,7 @@ class Labeler():
             coef_list.append(adj_cp_model.coef_)
             y_pred_list.append(y_pred)
         return np.asarray(coef_list), np.asarray(turning_points), y_pred_list, normalized_coef_list
+
     def plot(self,tics,parameters):
         if self.method=='linear':
             try:
@@ -197,9 +239,10 @@ class Labeler():
                 raise Exception("parameters shoud be [low,high] where the series would be split into 4 regimes by low,high and 0 as threshold based on slope. A value of -0.5 and 0.5 stand for -0.5% and 0.5% change per step.")
             for tic in tics:
                 self.linear_regession_plot(self.data_dict[tic],tic,self.y_pred_dict[tic],self.turning_points_dict[tic],low,high,normalized_coef_list=self.norm_coef_list_dict[tic])
+            self.TSNE_plot(self.tsne_results,self.all_label_seg)
     def linear_regession_plot(self,data, tic, y_pred_list, turning_points, low, high, normalized_coef_list):
         data = data.reset_index(drop=True)
-        fig, ax = plt.subplots(2, 1, figsize=(20, 10), constrained_layout=True)
+        fig, ax = plt.subplots(3, 1, figsize=(20, 10), constrained_layout=True)
         ax[0].plot([i for i in range(data.shape[0])], data['adjcp_filtered'])
         seg1, seg2, seg3 = sorted([low, high, 0])
         colors = list(dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS).keys())
@@ -207,15 +250,9 @@ class Labeler():
             x_seg = np.asarray([j for j in range(turning_points[i], turning_points[i + 1])]).reshape(-1, 1)
             y_pred = y_pred_list[i]
             coef = normalized_coef_list[i]
-            if coef <= seg1:
-                flag = 0
-            elif coef > seg1 and coef <= seg2:
-                flag = 1
-            elif coef > seg2 and coef <= seg3:
-                flag = 2
-            elif coef > seg3:
-                flag = 3
+            flag=self.regime_flag(self.regime_number,coef,[seg1, seg2, seg3])
             ax[1].plot(x_seg, y_pred, color=colors[flag], label='cat' + str(flag))
+            ax[2].plot(x_seg,data['adjcp'].iloc[turning_points[i]:turning_points[i + 1]], color=colors[flag], label='cat' + str(flag))
         handles, labels = plt.gca().get_legend_handles_labels()
         by_label = dict(zip(labels, handles))
         plt.legend(by_label.values(), by_label.keys())
@@ -223,6 +260,7 @@ class Labeler():
         if not os.path.exists('res/linear_model/'):
             os.makedirs('res/linear_model/')
         fig.savefig('res/linear_model/' + tic + '.png')
+        plt.close(fig)
 
     def linear_regession_timewindow(self,data_ori, tic, adjcp_timewindow):
         # This is the version of linear regession that does not use a turning point to segment. Instead, it applys a fixed-length time winodw.
@@ -241,3 +279,34 @@ class Labeler():
             ax[1].plot([i * adjcp_timewindow + j for j in range(adjcp_timewindow)], y_pred)
             coef_list.append(adj_cp_model.coef_)
         return coef_list
+
+    def interpolation(self,data):
+        max_len = max([len(d) for d in data])
+        for i, d in enumerate(data):
+            l = len(d)
+            to_fill = max_len - l
+            if to_fill != 0:
+                interval = max_len // to_fill
+                for j in range(to_fill):
+                    idx = (interval + 1) * j + interval
+                    data[i].insert(min(idx, len(data[i]) - 1), float('nan'))
+            data[i] = pd.Series(data[i]).interpolate(method='polynomial', order=2)
+        return data
+
+    def TSNE_run(self,data_seg):
+        interpolated_pct_return_data_seg = np.array(self.interpolation(data_seg))
+        self.tsne = TSNE(n_components=2, perplexity=40, n_iter=300)
+        self.tsne_results = self.tsne.fit_transform(interpolated_pct_return_data_seg)
+
+    def TSNE_plot(self,data, label_list):
+        colors = list(dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS).keys())
+        fig, ax = plt.subplots(1, 1, figsize=(20, 10), constrained_layout=True)
+        for i in range(len(data) - 1):
+            label = label_list[i]
+            ax.scatter(data[i][0], data[i][1], color=colors[label], alpha=0.2, label='cluster' + str(label))
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys())
+        plt.title('TSNE', fontsize=20)
+        fig.savefig('res/linear_model/TSNE.png')
+        plt.close(fig)
