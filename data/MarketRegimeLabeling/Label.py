b1bdab8870aba566f9070a23104890316424546a
Xiahaochong98 <xiahaochong981002@gmail.com>
2022-12-30 23:16:17 +0800

    add evaluation for EIIE

--- data/MarketRegimeLabeling/Label.py
+++ data/MarketRegimeLabeling/Label.py
@@ -1,7 +1,7 @@
 import labeling_util as util
 import argparse
 import pandas as pd
-
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", type=str)
@@ -10,9 +10,16 @@ parser.add_argument("--fitting_parameters",nargs='+', type=str)
 parser.add_argument("--labeling_parameters",  nargs="+", type=float)
 parser.add_argument('--regime_number',type=int,default=4)
 parser.add_argument('--length_limit',type=int,default=0)
+parser.add_argument('--OE_BTC',type=bool,default=False)
 args= parser.parse_args()
 
 print('labeling start')
+if args.OE_BTC==True:
+    raw_data=pd.read_csv(args.data_path)
+    raw_data['tic']='OE_BTC'
+    raw_data['adjcp']=raw_data["midpoint"]
+    raw_data.to_csv('/temp/OE_BTC_processed.csv')
+    args.data_path='/temp/OE_BTC_processed.csv'
 Labeler=util.Labeler(args.data_path,'linear')
 Labeler.fit(args.regime_number,args.length_limit)
 Labeler.label(args.labeling_parameters)
@@ -24,3 +31,5 @@ print('labeling done')
 print('plotting start')
 Labeler.plot(Labeler.tics,args.labeling_parameters,args.data_path)
 print('plotting done')
+if args.OE_BTC==True:
+    os.remove('/temp/OE_BTC_processed.csv')
