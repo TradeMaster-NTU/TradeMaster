8dc7bb0f4199cf1a30e530113b4c1ef9f2c52175
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-02-26 15:07:22 +0800

    update hft support and docs

--- trademaster/trainers/portfolio_management/eiie_trainer.py
+++ trademaster/trainers/portfolio_management/eiie_trainer.py
@@ -217,10 +217,11 @@ class PortfolioManagementEIIETrainer(Trainer):
             tensor_state = torch.as_tensor(state, dtype=torch.float32, device=self.device).unsqueeze(0)
             # print('extra_parameters is ',extra_parameters)
             if customize_policy_id=="Average_holding":
-                action = policy(tensor_state, self.test_environment,return_dict["weights_brandnew"])
+                action = policy(tensor_state, self.test_environment,weights_brandnew)
             else:
                 action = policy(tensor_state, self.test_environment)
             state, reward, done, return_dict = self.test_environment.step(action)
+            weights_brandnew=return_dict["weights_brandnew"]
             episode_reward_sum += reward
             if done:
                 print("Test Best Episode Reward Sum: {:04f}".format(episode_reward_sum))
