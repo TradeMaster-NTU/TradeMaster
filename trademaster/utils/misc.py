2c5658b0ef563d295c78afe1de13782cfb7289d7
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-05-12 16:29:51 +0800

    add train/test end signal in back_end service

--- trademaster/utils/misc.py
+++ trademaster/utils/misc.py
@@ -427,7 +427,7 @@ def load_model(output_dir,
                save,
                epoch = None,
                resume = None,
-               is_train = True):
+               is_train = True,verbose = False):
 
     if resume is None:
         resume = get_last_checkpoint(output_dir)
@@ -440,11 +440,13 @@ def load_model(output_dir,
     for name, model in save["models"].items():
         if model:
             model.load_state_dict(checkpoint[name])
-    print("Resume checkpoint %s" % resume)
+    if verbose:
+        print("Resume checkpoint %s" % resume)
     if is_train:
         for name, optimizer in save["optimizers"].items():
             optimizer.load_state_dict(checkpoint[name])
-        print("With optim & sched!")
+        if verbose:
+            print("With optim & sched!")
 
 def load_best_model(output_dir,
                save,
