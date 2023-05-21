f5d27f8b589cd255ce401520d801d8174827b181
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-05-21 16:53:31 +0800

    add custom

---
+++ configs/_base_/market_dynamics_model/custom/custom/mdm.py
@@ -0,0 +1,14 @@
+market_dynamics_model = dict(
+    data_path='',
+fitting_parameters=['2/7','2/14','4'],
+labeling_parameters=[-0.05,0.05],
+regime_number=3,
+length_limit=24,
+OE_BTC=False,
+PM='',
+process_datafile_path='',
+market_dynamic_labeling_visualization_paths='',
+key_indicator='adjcp',
+timestamp='date',
+tic='tic'
+)
