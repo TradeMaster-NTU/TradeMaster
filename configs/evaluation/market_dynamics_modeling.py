500ba0644bdf51ddafefb996567f3f43d789ac6c
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-05-25 15:33:49 +0800

    add https://datasets.tardis.dev/v1/binance-futures/book_snapshot_5/2020/09/01/BTCUSDT.csv.gz dataset for MDM

--- configs/evaluation/market_dynamics_modeling.py
+++ configs/evaluation/market_dynamics_modeling.py
@@ -1,5 +1,5 @@
-task_name = "algorithmic_trading"
-dataset_name = "BTC"
+task_name = "custom"
+dataset_name = "custom"
 
 
 _base_ = [
@@ -8,21 +8,21 @@ _base_ = [
 
 market_dynamics_model = dict(
     type='Linear_Market_Dynamics_Model',
-    data_path="data/algorithmic_trading/BTC/data.csv",
+    data_path="data/market_dynamics_modeling/binance-futures_book_snapshot_5_2020-09-01_BTCUSDT.csv",
 filter_strength=1,
-slope_interval=[-0.15,0.15],
+slope_interval=[-0.01,0.01],
 dynamic_number=5,
-max_length_expectation=100,
+max_length_expectation=3600,
 OE_BTC=False,
 PM='',
 process_datafile_path='',
 market_dynamic_labeling_visualization_paths='',
-key_indicator='adjcp',
-timestamp='date',
-tic='tic',
+key_indicator='bid1_price',
+timestamp='timestamp',
+tic='BTCUSDT',
 labeling_method='quantile',
-min_length_limit=24,
+min_length_limit=60,
 merging_metric='DTW_distance',
-merging_threshold=0.005,
+merging_threshold=0.0003,
 merging_dynamic_constraint=1
 )
