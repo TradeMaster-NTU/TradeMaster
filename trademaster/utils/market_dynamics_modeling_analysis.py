294bc310de416b71411850ecbfc16e08151ee042
Xiahaochong98 <xiahaochong981002@gmail.com>
2023-05-29 21:59:14 +0800

    add TradeMaster Sandbox

--- trademaster/utils/market_dynamics_modeling_analysis.py
+++ trademaster/utils/market_dynamics_modeling_analysis.py
@@ -123,9 +123,9 @@ class MarketDynamicsModelingAnalysis(object):
                 dynamic_data_seg = dynamic_data.iloc[interval[0]:interval[1], :]
                 # save segmented data to file
                 if self.file_extension == 'feather':
-                    dynamic_data_seg.reset_index().to_feather(data_folder + '/label_' + str(i) + '/label_' + str(i) + '_' + str(index) + '.' + extension)
+                    dynamic_data_seg.reset_index().to_feather(data_folder + '/label_' + str(i) + '/label_' + str(i) + '_' + str(index) + '.' + self.file_extension)
                 elif self.file_extension == 'csv':
-                    dynamic_data_seg.to_csv(data_folder + '/label_' + str(i)  + '/label_' + str(i) + '_' + str(index) +'.'+extension)
+                    dynamic_data_seg.to_csv(data_folder + '/label_' + str(i)  + '/label_' + str(i) + '_' + str(index) +'.'+self.file_extension)
         return data_folder,len(dynamics)
 
 
