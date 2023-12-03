786caaccd8c0a4e0bff3d9099167e2341defb2be
ai-gamer <sunshuo1201@gmail.com>
2023-12-03 13:56:43 +0800

    Update builder.py

--- trademaster/environments/builder.py
+++ trademaster/environments/builder.py
@@ -3,7 +3,6 @@ from trademaster.utils import build_from_cfg
 import copy
 
 ENVIRONMENTS = Registry('environment')
-
 def build_environment(cfg, default_args=None):
     cp_cfg = copy.deepcopy(cfg.environment)
     environment = build_from_cfg(cp_cfg, ENVIRONMENTS, default_args)
